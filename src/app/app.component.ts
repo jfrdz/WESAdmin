cd54210bea5265ea2f178320b96f1ff42f303750
ckenox <jfrodriguez@hotmail.es>
2022-02-10 15:09:03 -0600

    repo inicial con archivos agregados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,77 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'WESAdmin';
+  newMemberName = '';
+  members: string[] = [];
+  errorMsg = "";
+  numberOfTeams:number | "" = "";
+  teams:string[][] = []
+
+  onInput(member:string)
+  {
+    this.newMemberName = member;
+    console.log(this.newMemberName);
+  }
+
+  onInputTeams(value:string)
+  {
+    this.numberOfTeams = Number(value);
+    console.log(this.numberOfTeams);
+  }
+
+  addMember()
+  {
+    if(!this.newMemberName)
+    {
+      this.errorMsg = "Name can't be empty";
+      return;
+    }  
+    this.errorMsg = '';
+
+    this.members.push(this.newMemberName);
+    this.newMemberName = "";
+    console.log(this.members);
+  }
+
+  generateTeams()
+  {
+    if(!this.numberOfTeams || this.numberOfTeams <= 0)
+    {
+      this.errorMsg ="Wrong number of teams";
+      return;
+    }
+
+    if(this.members.length < this.numberOfTeams)
+    {
+      this.errorMsg ="Wrong number of teams";
+      return;
+    }
+    const allMembers = [...this.members];
+
+    while(allMembers.length)
+    {    
+      for(let i=0; i<this.numberOfTeams; i++)
+      {
+        const randomIndex = Math.floor(Math.random()* allMembers.length) ;
+        const member = allMembers.splice(randomIndex,1)[0];
+        
+        if(!member)
+          break;
+
+        if(this.teams[i])
+        {
+          this.teams[i].push(member);
+        }
+        else
+        {
+          this.teams[i] = [member];
+        }
+      }
+    }
+
+    this.members = [];
+    this.numberOfTeams = "";
+
+  }
+
 }
